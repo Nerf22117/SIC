23e9585dc24854a696d549dc9006b398a1d666e9
Nerf22117 <40220191@esmad.ipp.pt>
2025-01-25 17:02:36 +0000

    Add password reset functionality with validation and email notifications

--- api/resolvers/user.resolver.js
+++ api/resolvers/user.resolver.js
@@ -157,6 +157,113 @@ const userResolver = {
         throw new Error(error.message || "Internal Server Error");
       }
     },
+    forgotPassword: async (_, { email }) => {
+      try {
+        if (!email) {
+          throw new Error("Please fill all fields");
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        if (!user.isVerified) {
+          throw new Error(
+            "Your account is not verified. Please validate your account."
+          );
+        }
+
+        const saltNumber = parseInt(process.env.SALT_NUMBER);
+        const salt = await bcrypt.genSalt(saltNumber);
+        const resetPasswordCode = generateOtp();
+        const hashedResetPasswordCode = await bcrypt.hash(
+          resetPasswordCode,
+          salt
+        );
+
+        user.resetPasswordCode = hashedResetPasswordCode;
+        await user.save();
+
+        await sendOtpEmail(email, resetPasswordCode, "passwordReset");
+
+        return { message: "Reset password code sent to your email" };
+      } catch (error) {
+        console.error("Error in forgotPassword: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+    validateResetPasswordCode: async (_, { email, resetPasswordCode }) => {
+      try {
+        if (!email || !resetPasswordCode) {
+          throw new Error("Please fill all fields");
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        if (!user.resetPasswordCode) {
+          throw new Error("Reset password code not found");
+        }
+
+        const validResetPasswordCode = await bcrypt.compare(
+          resetPasswordCode,
+          user.resetPasswordCode
+        );
+
+        if (!validResetPasswordCode) {
+          throw new Error("Invalid reset password code");
+        }
+
+        user.resetPasswordCode = null;
+        await user.save();
+
+        return {
+          message:
+            "Reset code validated successfully. You can now reset your password",
+        };
+      } catch (error) {
+        console.error("Error in validateResetPasswordCode: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+    resetPassword: async (_, { email, newPassword, confirmNewPassword }) => {
+      try {
+        if (!email || !newPassword || !confirmNewPassword) {
+          throw new Error("Please fill all fields");
+        }
+
+        if (newPassword !== confirmNewPassword) {
+          throw new Error("Passwords do not match");
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        if (user.resetPasswordCode) {
+          throw new Error("Verify your reset password code first");
+        }
+
+        const saltNumber = parseInt(process.env.SALT_NUMBER);
+        const salt = await bcrypt.genSalt(saltNumber);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+        user.password = hashedPassword;
+        await user.save();
+
+        return {
+          message:
+            "Password reset successful. Redirecting you to the login page...",
+        };
+      } catch (error) {
+        console.error("Error in resetPassword: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
   },
   Query: {
     authUser: async (_, __, context) => {

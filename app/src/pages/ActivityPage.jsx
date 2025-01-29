8b72eb4795548b47154199c0c00025d4d927ea51
Pedro <40220243@esmad.ipp.pt>
2025-01-29 22:23:54 +0000

    style: enhance layout and styling of Exercises and Profile pages

--- app/src/pages/ActivityPage.jsx
+++ app/src/pages/ActivityPage.jsx
@@ -6,12 +6,14 @@ import { GET_USER_FOODS } from "../graphql/queries/food.query";
 import { GET_USER_EXERCISES, GET_EXERCISE_USER_CATEGORIES } from "../graphql/queries/exercise.query.js";
 import { toast } from "react-hot-toast";
 
-export default function FoodListPage() {
+export default function ActivityPage() {
     const [searchFood, setSearchFood] = useState("");
     const [orderFood, setOrderFood] = useState("");
+    const [foodPage, setFoodPage] = useState(1);
 
     const [searchExercise, setSearchExercise] = useState("");
     const [orderExercise, setOrderExercise] = useState("");
+    const [exercisePage, setExercisePage] = useState(1);
 
     const [muscularGroup, setMuscularGroup] = useState("");
 
@@ -22,8 +24,8 @@ export default function FoodListPage() {
             input: { userId: authUser?._id },
             search: searchFood,
             order: orderFood || null,
-            limit: 10,
-            offset: 0,
+            limit: 4,
+            offset: (foodPage - 1) * 4,
         },
     });
 
@@ -32,8 +34,8 @@ export default function FoodListPage() {
             input: { userId: authUser?._id },
             search: searchExercise,
             order: orderExercise || null,
-            limit: 10,
-            offset: 0,
+            limit: 4,
+            offset: (exercisePage - 1) * 4,
             category: muscularGroup === "All" ? "" : muscularGroup,
         },
     });
@@ -44,29 +46,30 @@ export default function FoodListPage() {
 
     useEffect(() => {
         refetchUserFood();
-    }, [searchFood, orderFood, refetchUserFood]);
+    }, [searchFood, orderFood, foodPage, refetchUserFood]);
 
     useEffect(() => {
         refetchUserExercise();
-    }, [searchExercise, orderExercise, dataUserExercise, refetchUserExercise, muscularGroup]);
+    }, [searchExercise, orderExercise, exercisePage, muscularGroup, refetchUserExercise]);
 
     if (loadingUserFood || loadingUserExercise) return <p>Loading...</p>;
     if (errorUserFood || errorUserExercise) return toast.error("An error occurred. Please try again.");
 
     return (
-        <div >
-            <div className="p-6">
-                <h1 className="text-2xl font-bold mb-4">Activity</h1>
-                <div className="flex flex-col md:flex-row md:items-center md:space-x-4 mb-4">
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-2">Activity</h1>
+            <div className="mb-2">
+                <h2 className="text-xl font-bold mb-2">Food</h2>
+                <div className="flex flex-col md:flex-row md:items-center md:space-x-4 mb-2">
                     <input
                         type="text"
                         placeholder="Search for food..."
-                        className="border border-gray-300 p-2 w-full md:w-1/1 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4 md:mb-0"
+                        className="border border-gray-300 p-2 w-full md:w-2/3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2 md:mb-0"
                         value={searchFood}
                         onChange={(e) => setSearchFood(e.target.value)}
                     />
                     <select
-                        className="border border-gray-300 p-2 w-full md:w-1/6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="border border-gray-300 p-2 w-full md:w-1/3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         value={orderFood}
                         onChange={(e) => setOrderFood(e.target.value)}
                     >
@@ -79,43 +82,56 @@ export default function FoodListPage() {
                         <option value="QUANTITY_DESC">Quantity (Highest to Lowest)</option>
                     </select>
                 </div>
-
-                <div className="mt-4 h-140 overflow-y-auto border border-gray-300 rounded-lg">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                        {dataUserFood?.getUserFoods?.result?.length > 0 ? (
-                            dataUserFood.getUserFoods.result.map((food) => (
-                                <div key={food._id} className="border border-gray-300 rounded-lg p-4 hover:bg-gray-100">
-                                    <Link to={`/food/${food.foodId}`}>
-                                        <img
-                                            src={food.image}
-                                            alt={food.name}
-                                            className="w-full h-32 object-cover rounded-lg mb-4"
-                                        />
-                                        <h2 className="text-xl font-bold">{food.name}</h2>
-                                        <p className="text-gray-500">{food.category}</p>
-                                        <p className="text-gray-500">{food.calories} calories</p>
-                                        <p className="text-gray-500">Quantity: {food.quantity}</p>
-                                    </Link>
-                                </div>
-                            ))
-                        ) : (
-                            <p className="p-4 text-center text-gray-500">No results found.</p>
-                        )}
-                    </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                    {dataUserFood?.getUserFoods?.result?.length > 0 ? (
+                        dataUserFood.getUserFoods.result.map((food) => (
+                            <div key={food._id} className="border border-gray-300 rounded-lg p-2 hover:bg-gray-100">
+                                <Link to={`/food/${food.foodId}`}>
+                                    <img
+                                        src={food.image}
+                                        alt={food.name}
+                                        className="w-full h-24 object-cover rounded-lg mb-2"
+                                    />
+                                    <h2 className="text-lg font-bold">{food.name}</h2>
+                                    <p className="text-gray-500 text-sm">{food.category}</p>
+                                    <p className="text-gray-500 text-sm">{food.calories} calories</p>
+                                    <p className="text-gray-500 text-sm">Quantity: {food.quantity}</p>
+                                </Link>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="p-4 text-center text-gray-500">No results found.</p>
+                    )}
+                </div>
+                <div className="flex justify-center mt-2">
+                    <button
+                        className="px-3 py-1 bg-gray-300 text-gray-700 rounded-lg mr-2"
+                        onClick={() => setFoodPage((prev) => Math.max(prev - 1, 1))}
+                        disabled={foodPage === 1}
+                    >
+                        Previous
+                    </button>
+                    <button
+                        className="px-3 py-1 bg-gray-300 text-gray-700 rounded-lg"
+                        onClick={() => setFoodPage((prev) => prev + 1)}
+                        disabled={dataUserFood?.getUserFoods?.result?.length < 4}
+                    >
+                        Next
+                    </button>
                 </div>
             </div>
-            <div className="p-6">
-                <h1 className="text-2xl font-bold mb-4">Exercises</h1>
-                <div className="flex flex-col md:flex-row md:items-center md:space-x-4 mb-4">
+            <div>
+                <h2 className="text-xl font-bold mb-2">Exercises</h2>
+                <div className="flex flex-col md:flex-row md:items-center md:space-x-4 mb-2">
                     <input
                         type="text"
                         placeholder="Search for exercise..."
-                        className="border border-gray-300 p-2 w-full md:w-1/1 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4 md:mb-0"
+                        className="border border-gray-300 p-2 w-full md:w-2/3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2 md:mb-0"
                         value={searchExercise}
                         onChange={(e) => setSearchExercise(e.target.value)}
                     />
                     <select
-                        className="border border-gray-300 p-2 w-full md:w-1/6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4 md:mb-0"
+                        className="border border-gray-300 p-2 w-full md:w-1/3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2 md:mb-0"
                         value={muscularGroup}
                         onChange={(e) => setMuscularGroup(e.target.value)}
                     >
@@ -127,7 +143,7 @@ export default function FoodListPage() {
                         ))}
                     </select>
                     <select
-                        className="border border-gray-300 p-2 w-full md:w-1/6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="border border-gray-300 p-2 w-full md:w-1/3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         value={orderExercise}
                         onChange={(e) => setOrderExercise(e.target.value)}
                     >
@@ -138,27 +154,40 @@ export default function FoodListPage() {
                         <option value="CALORIES_DESC">Calories (Highest to Lowest)</option>
                     </select>
                 </div>
-
-                <div className="mt-4 h-140 overflow-y-auto border border-gray-300 rounded-lg">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-                        {dataUserExercise?.getUserExercises?.result?.length > 0 ? (
-                            dataUserExercise.getUserExercises.result.map((exercise) => (
-                                <div key={exercise._id} className="border border-gray-300 rounded-lg p-4 hover:bg-gray-100">
-                                    <img
-                                        src={exercise.gif}
-                                        alt={exercise.activity}
-                                        className="w-full h-32 object-cover rounded-lg mb-4"
-                                    />
-                                    <h2 className="text-xl font-bold">{exercise.activity}</h2>
-                                    <p className="text-gray-500">{exercise.duration} minutes</p>
-                                    <p className="text-gray-500">{exercise.calories} calories</p>
-                                    <p className="text-gray-500">Muscular Group: {exercise.muscularGroup}</p>
-                                </div>
-                            ))
-                        ) : (
-                            <p className="p-4 text-center text-gray-500">No results found.</p>
-                        )}
-                    </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                    {dataUserExercise?.getUserExercises?.result?.length > 0 ? (
+                        dataUserExercise.getUserExercises.result.map((exercise) => (
+                            <div key={exercise._id} className="border border-gray-300 rounded-lg p-2 hover:bg-gray-100">
+                                <img
+                                    src={exercise.gif}
+                                    alt={exercise.activity}
+                                    className="w-full h-24 object-cover rounded-lg mb-2"
+                                />
+                                <h2 className="text-lg font-bold">{exercise.activity}</h2>
+                                <p className="text-gray-500 text-sm">{exercise.duration} minutes</p>
+                                <p className="text-gray-500 text-sm">{exercise.calories} calories</p>
+                                <p className="text-gray-500 text-sm">Muscular Group: {exercise.muscularGroup}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="p-4 text-center text-gray-500">No results found.</p>
+                    )}
+                </div>
+                <div className="flex justify-center mt-4">
+                    <button
+                        className="px-3 py-1 bg-gray-300 text-gray-700 rounded-lg mr-2"
+                        onClick={() => setExercisePage((prev) => Math.max(prev - 1, 1))}
+                        disabled={exercisePage === 1}
+                    >
+                        Previous
+                    </button>
+                    <button
+                        className="px-3 py-1 bg-gray-300 text-gray-700 rounded-lg"
+                        onClick={() => setExercisePage((prev) => prev + 1)}
+                        disabled={dataUserExercise?.getUserExercises?.result?.length < 4}
+                    >
+                        Next
+                    </button>
                 </div>
             </div>
         </div>
--- app/src/pages/ProfilePage.jsx
+++ app/src/pages/ProfilePage.jsx
@@ -1,21 +1,15 @@
 import { useState, useEffect } from "react";
-
 import { useMutation, useQuery } from "@apollo/client";
 import { GET_AUTHENTICATED_USER } from "../graphql/queries/user.query";
 import { UPDATE_USER } from "../graphql/mutations/user.mutation";
-
 import toast from "react-hot-toast";
-
 import SelectField from "../components/ui/SelectField";
 import InputField from "../components/ui/InputField";
 
 export default function ProfilePage() {
   const { data, loading, error } = useQuery(GET_AUTHENTICATED_USER);
 
-  console.log(data);
-
-  const [updateUser, { loading: updating, error: updateError }] =
-    useMutation(UPDATE_USER);
+  const [updateUser, { loading: updating, error: updateError }] = useMutation(UPDATE_USER);
 
   const [formData, setFormData] = useState({
     username: "",
@@ -68,8 +62,6 @@ export default function ProfilePage() {
       return;
     }
 
-    console.log(typeof changedFields.height);
-
     try {
       const response = await updateUser({
         variables: {
@@ -90,123 +82,104 @@ export default function ProfilePage() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="p-6 max-w-xl mx-auto bg-white shadow-lg rounded-lg border border-gray-200">
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
+    <div className="p-4 max-w-3xl mx-auto bg-white shadow-lg rounded-lg border border-gray-200">
+      <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
         Update your information
       </h1>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <InputField
-            label="Username"
-            id="username"
-            name="username"
-            type="text"
-            value={formData.username}
-            onChange={handleChange}
-          />
+      <form onSubmit={handleSubmit} className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <InputField
+          label="Username"
+          id="username"
+          name="username"
+          type="text"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Name"
+          id="name"
+          name="name"
+          type="text"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Password"
+          id="password"
+          name="password"
+          placeholder="********"
+          type="password"
+          value={formData.password || ""}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <SelectField
+          label="Gender"
+          id="gender"
+          name="gender"
+          value={formData.gender}
+          onChange={handleChange}
+          options={[
+            { label: "Male", value: "male" },
+            { label: "Female", value: "female" },
+          ]}
+        />
+        <InputField
+          label="Age"
+          id="age"
+          name="age"
+          type="number"
+          value={formData.age}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Weight (kg)"
+          id="weight"
+          name="weight"
+          type="number"
+          value={formData.weight}
+          onChange={handleChange}
+        />
+        <InputField
+          label="Height (cm)"
+          id="height"
+          name="height"
+          type="number"
+          value={formData.height}
+          onChange={handleChange}
+        />
+        <SelectField
+          label="Activity Level"
+          id="activity"
+          name="activity"
+          value={formData.activity}
+          onChange={handleChange}
+          options={[
+            { label: "Sedentary", value: "sedentary" },
+            { label: "Lightly Active", value: "lightly active" },
+            { label: "Moderately Active", value: "moderately active" },
+            { label: "Very Active", value: "very active" },
+          ]}
+        />
+        <div className="col-span-1 md:col-span-2">
+          <button
+            type="submit"
+            disabled={!isDirty || updating}
+            className="w-full bg-gradient-to-r from-blue-500 to-blue-700 text-white font-medium px-4 py-2 rounded-lg hover:shadow-md transition disabled:opacity-50 cursor-pointer"
+          >
+            {updating ? "Saving..." : "Save changes"}
+          </button>
+          {updateError && (
+            <p className="text-red-500 mt-2">Error: {updateError.message}</p>
+          )}
         </div>
-        <div>
-          <InputField
-            label="Name"
-            id="name"
-            name="name"
-            type="text"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <InputField
-            label="Password"
-            id="password"
-            name="password"
-            placeholder="********"
-            type="password"
-            value={formData.password || ""}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <InputField
-            label="Email"
-            id="email"
-            name="email"
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <SelectField
-            label="Gender"
-            id="gender"
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange}
-            options={[
-              { label: "Male", value: "male" },
-              { label: "Female", value: "female" },
-            ]}
-          />
-        </div>
-        <div>
-          <InputField
-            label="Age"
-            id="age"
-            name="age"
-            type="number"
-            value={formData.age}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <InputField
-            label="Weight (kg)"
-            id="weight"
-            name="weight"
-            type="number"
-            value={formData.weight}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <InputField
-            label="Height (cm)"
-            id="height"
-            name="height"
-            type="number"
-            value={formData.height}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <SelectField
-            label="Activity Level"
-            id="activity"
-            name="activity"
-            value={formData.activity}
-            onChange={handleChange}
-            options={[
-              { label: "Sedentary", value: "sedentary" },
-              { label: "Lightly Active", value: "lightly active" },
-              {
-                label: "Moderately Active",
-                value: "moderately active",
-              },
-              { label: "Very Active", value: "very active" },
-            ]}
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={!isDirty || updating}
-          className="w-full bg-gradient-to-r from-blue-500 to-blue-700 text-white font-medium px-4 py-2 rounded-lg hover:shadow-md transition disabled:opacity-50 cursor-pointer"
-        >
-          {updating ? "Saving..." : "Save changes"}
-        </button>
-        {updateError && (
-          <p className="text-red-500 mt-2">Error: {updateError.message}</p>
-        )}
       </form>
     </div>
   );
